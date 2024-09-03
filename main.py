5643d3a4e8302bdbbb83bcc84509f37152457018
Thomas <tlaskowski7375@gmail.com>
2024-09-03 17:56:39 -0400

    Added XP and Leveling System
    xp and leveling system
    still has to be balanced LOL

--- main.py
+++ main.py
@@ -1,148 +1,147 @@
 import random
-import streamlit as st
-import time
-import pandas as pd
-import numpy as np
+import math
+
+# Load all usernames from the file
+with open("data.txt", "r") as fp:
+    # Extract only the usernames (first item of each line)
+    names = [line.strip().split(",")[0] for line in fp]
 
 
-# vector = ["name", "level", "questions solved"];
-# data.txt is the name of where the user data will be stored
-# loading the user in with their data
+class SoloLeveling:
+    def __init__(self):
+        self.User = None
+        self.Level = 1
+        self.questionsSolved = 0
+        self.xp = 0
+        self.required_xp = int(pow((self.Level + 1 / 0.07), 2) + math.log10(self.Level + 1))
+        print(
+            "Welcome to Math Off\nIn this program you will solve math problems to increase your mental math abilities.\n"
+            "I wish you the best of luck in your journey and hope the best for you.")
 
-def askQuestion():
-    thing = []
-    qtype = random.randint(0, 3)
+    # This Function creates a new user in the Data in the TXT File.
+    def createData(self, user: str):
+        with open("data.txt", "a") as addData:
+            Level = 0
+            questionsSolved = 0
+            xp = 0
+            addData.write(f"{user},{Level},{xp},{questionsSolved}\n")
 
-    numberOne = 1
-    numberTwo = 11
+    def getData(self, user: str):
+        with open("data.txt", "r") as openData:
+            for userdata in openData:
+                vector = userdata.strip().split(",")
+                if vector[0] == user:
+                    print("User found!")
+                    self.User = user
+                    self.Level = int(vector[1].strip())
+                    self.xp = int(vector[2].strip())
+                    self.questionsSolved = int(vector[3].strip())
+                    self.required_xp = int(pow((self.Level + 1 / 0.07), 2) + math.log10(self.Level + 1))
+                    print(self.required_xp)
+                    print("Data Loaded.")
+                    return
+            print("User not found in data.txt.")
 
-    if qtype == 2:
-        while numberOne % numberTwo != 0:
-            numberOne = random.randint(1, 10)
-            numberTwo = random.randint(1, 10)
-    else:
-        while numberOne < numberTwo:
-            numberOne = random.randint(1, 10)
-            numberTwo = random.randint(1, 10)
+    def getUserInfo(self):
+        LoginType = input("L for Login, N for Create User\n")
+        if LoginType.lower() == "l":
+            print("What is your username?")
+            getuser = input("Type your username: ")
+            if getuser in names:
+                print("Loading your data")
+                self.getData(getuser)
+            else:
+                print("Username not found.")
+                for i in names:
+                    print(i)
+                newortry = input("Try Again (T) or Create New Account (N)?")
+                if newortry.lower() == "t":
+                    print("What is your username?")
+                    getuser = input("Type your username: ")
+                    if getuser in names:
+                        print("Loading your data")
+                        self.getData(getuser)
+                    else:
+                        print("Restart Program try again.")
+                if newortry.lower() == "n":
+                    newUsername = input("What is the Username you want to use? : ")
+                    self.createData(newUsername)
+                    self.getData(newUsername)
+        elif LoginType.lower() == "n":
+            newUsername = input("What is the Username you want to use? : ")
+            self.createData(newUsername)
+            self.getData(newUsername)
 
-    typeShit = [(numberOne + numberTwo), (numberOne * numberTwo), (numberOne / numberTwo), (numberOne - numberTwo)]
-    problemType = ["+", "x", "/", "-"]
-    realAnswer = typeShit[qtype]
-    thing.append(realAnswer)
-    thing.append(numberOne)
-    thing.append(numberTwo)
-    thing.append(problemType[qtype])
-    return thing
+    def displayStats(self):
+        print(f"Your current level is {self.Level}, and you have solved {self.questionsSolved} questions. \n"
+              f"You need {self.required_xp - self.xp} more xp to level up ({self.xp} / {self.required_xp}). ")
+
+    def askQuestion(self):
+        qtype = random.randint(0, 3)
+        numberOne = 1
+        numberTwo = 11
+
+        if qtype == 2:
+            while numberOne % numberTwo != 0:
+                numberOne = random.randint(1, 10)
+                numberTwo = random.randint(1, 10)
+        else:
+            while numberOne < numberTwo:
+                numberOne = random.randint(1, 10)
+                numberTwo = random.randint(1, 10)
+
+        typeShit = [(numberOne + numberTwo), (numberOne * numberTwo), (numberOne / numberTwo), (numberOne - numberTwo)]
+        problemType = ["+", "*", "/", "-"]
+        realAnswer = typeShit[qtype]
+        userAnswer = -1
+
+        while userAnswer != realAnswer:
+            userAnswer = int(input(f"{numberOne} {problemType[qtype]} {numberTwo} ?\n"))
+            if userAnswer == realAnswer:
+                print("Correct!")
+                return True
+            else:
+                print("Incorrect")
+
+    def saveData(self, solvedQuestions: int, levelup: bool):
+        with open("data.txt", "r") as fp:
+            lines = fp.readlines()
+
+        with open("data.txt", "w") as fp:
+            for userprofile in lines:
+                temp = userprofile.strip().split(",")
+                if not levelup:
+                    temp[2] = str((int(temp[2].strip()) + (int((math.sqrt(self.required_xp) * int(solvedQuestions))))))
+                    temp[3] = str(int(temp[3].strip()) + int(solvedQuestions))
+                else:
+                    temp[1] = str(self.Level)
+                    temp[2] = str(0)
+                    temp[3] = str(int(temp[3].strip()) + int(solvedQuestions))
+                fp.write(",".join(temp) + "\n")
+
+    # Required Function Should Be Altered After Play Testing
+    def checkLevelUp(self):
+        print(self.required_xp)
+        if self.xp >= self.required_xp:
+            print(f"Leveled Up!\n You are now level {self.Level + 1}!")
+            self.xp = 0
+            self.Level = self.Level + 1
+            return True
+        else:
+            return False
+
+    def startSession(self, qAmount: int):
+        for _ in range(qAmount):
+            self.askQuestion()
+        self.questionsSolved += qAmount
+        if self.checkLevelUp():
+            self.saveData(qAmount, True)
+        else:
+            self.saveData(qAmount, False)
+        self.displayStats()
 
 
-# Main app
-st.write("""
-# Welcome to Math Off!
-Made by *Thomas Laskowski*
-""")
-
-st.write("My Website [:link:](%s)" % "https://tomski.work/")
-
-x = st.write("")
-
-if st.button("Generate Problem"):
-    vals = askQuestion()
-    x = st.write(f"<p style='font-size:96px;'>{vals[1]} {vals[3]} {vals[2]}</p>", unsafe_allow_html=True)
-
-
-
-# with open("users.txt", "r") as fp:
-#     names = [line.strip() for line in fp]
-
-
-# Functions to maybe use in the future?
-
-# class SoloLeveling:
-#     def __init__(self):
-#         self.User = None
-#         self.Level = 1
-#         self.questionsSolved = 0
-#         self.stats = [self.User, self.Level, self.questionsSolved]
-#         print("Welcome to Math Off\nIn this program you will solve math problems to increase your mental math "
-#               "abilities.\nI wish you the best of luck in your journey and hope the best for you.")
-# 
-#     def createData(self, user: str):
-#         with open("data.txt", "a") as addData:
-#             Level = 0
-#             questionsSolved = 0
-#             User = user
-#             addData.write(f"{User},{Level},{questionsSolved}\n")
-# 
-#     def getData(self, user: str):
-#         with open("data.txt", "r") as openData:
-#             for userdata in openData:
-#                 vector = userdata.strip().split(",")
-#                 if vector[0] == user:
-#                     print("User found!")
-#                     self.User = user
-#                     self.Level = int(vector[1])
-#                     self.questionsSolved = int(vector[2])
-#                     print("Data Loaded.")
-#                     return
-#             print("User not found in data.txt.")
-# 
-#     def getUserInfo(self, LoginType: str):
-#         if LoginType.lower() == "l":
-#             print("What is your username?")
-#             for users in names:
-#                 print(f"{users}")
-#             getuser = input("Type your username: ")
-#             if getuser in names:
-#                 print("Loading your data")
-#                 self.getData(getuser)
-#                 print("Done!")
-#             else:
-#                 print("Username not found.")
-#         elif LoginType.lower() == "n":
-#             newUsername = input("What is the Username you want to use? : ")
-#             self.createData(newUsername)
-#             self.getData(newUsername)
-# 
-#     def askQuestion(self):
-#         qtype = random.randint(0, 3)
-# 
-#         numberOne = 1
-#         numberTwo = 11
-# 
-#         if qtype == 2:
-#             while numberOne % numberTwo != 0:
-#                 numberOne = random.randint(1, 10)
-#                 numberTwo = random.randint(1, 10)
-#         else:
-#             while numberOne < numberTwo:
-#                 numberOne = random.randint(1, 10)
-#                 numberTwo = random.randint(1, 10)
-# 
-#         typeShit = [(numberOne + numberTwo), (numberOne * numberTwo), (numberOne / numberTwo), (numberOne - numberTwo)]
-#         problemType = ["+", "*", "/", "-"]
-#         realAnswer = typeShit[qtype]
-#         userAnswer = -1
-# 
-#         while userAnswer != realAnswer:
-#             userAnswer = int(input(f"{numberOne} {problemType[qtype]} {numberTwo} ?\n"))
-#             if userAnswer == realAnswer:
-#                 print("Correct!")
-#             else:
-#                 print("Incorrect")
-# 
-#     def saveData(self, solvedQuestions: int):
-#         with open("data.txt", "r") as fp:
-#             lines = fp.readlines()
-# 
-#         with open("data.txt", "w") as fp:
-#             for userprofile in lines:
-#                 temp = userprofile.strip().split(",")
-#                 if temp[0] == self.User:
-#                     temp[2] = str(int(temp[2]) + solvedQuestions)
-#                 fp.write(",".join(temp) + "\n")
-# 
-#     def startSession(self, qAmount: int):
-#         for i in range(qAmount):
-#             self.askQuestion()
-#         self.questionsSolved += qAmount
-#         self.saveData(qAmount)
+main = SoloLeveling()
+main.getUserInfo()
+main.startSession(10)
+main.checkLevelUp()
